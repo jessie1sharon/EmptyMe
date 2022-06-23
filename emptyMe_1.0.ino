87c1e1491a9e444631a617dbc956557b03dec0cf
jessie1sharon <83138460+jessie1sharon@users.noreply.github.com>
2022-06-23 12:11:13 +0300

    the final code

--- emptyMe_1.0.ino
+++ emptyMe_1.0.ino
@@ -4,10 +4,15 @@
 DS3231  rtc(SDA, SCL);
 Time t;
 
-const int OnHour = 10;  // clock settings
+SoftwareSerial sensor(12,11);
+SoftwareSerial blue(2,3);
+
+const int OnHour = 05;  // clock settings
 const int OnMin = 00;
-const int OnHour1 = 22;
+const int OnHour1 = 17;
 const int OnMin1 = 00;
+
+int state = 0;  //state for blue
 
 int trigPin = 12;     // distance settings
 int echoPin = 11;
@@ -20,18 +25,32 @@ int j;
 float bucket = 0;
 
 void setup() {
-pinMode(trigPin,OUTPUT);
-pinMode(echoPin,INPUT);
-Serial.begin(9600);
+pinMode(trigPin,OUTPUT);  // input to distance sensor
+pinMode(echoPin,INPUT);   // output from distance sensor
+sensor.begin(9600);
+blue.begin(9600);
+blue.println("Starting...");
+
 rtc.begin();
 
 }
 
-void loop() {
+void checkState(){
+  if (blue.available()> 0){
+    state = blue.read(); // Reads the data from the bluetooth
+}
+if (state == 'R'){
+    String StrUno = "average measure is ";  // send last average
+    String StrDos = String(StrUno + avMeas);
+    blue.println(StrDos);
+    state = 0;
+ }
+}
 
+void loop() {
 t = rtc.getTime();
 
-if (t.hour == OnHour && t.min == OnMin){
+if ((t.hour == OnHour && t.min == OnMin) || (t.hour == OnHour1 && t.min == OnMin1)){   // in certain times check if full
   for (j=1;j<=numMeas;j=j+1){
     digitalWrite(trigPin,LOW);
     delayMicroseconds(10);
@@ -39,27 +58,33 @@ if (t.hour == OnHour && t.min == OnMin){
     delayMicroseconds(10);
     digitalWrite(trigPin,LOW);
     pingTravelTime = pulseIn(echoPin,HIGH);
-    delay(1000);
-    pingTravelDistance = (pingTravelTime*765.*5280.*12.*2.54)/(3600.*1000000);
+    delay(1200);      // if full print once
+    pingTravelDistance = (pingTravelTime*765.*5280.*12.*2.54)/(3600.*1000000);  // measure distance in cm
     distanceToTarget = pingTravelDistance/2;
-    bucket = bucket+distanceToTarget;
+    bucket = bucket+distanceToTarget;   // measure multiple times to take the average distance
+    checkState();
   }
   avMeas = bucket / numMeas;
-  bucket = 0;
+  bucket = 0;   // for next measure
   
-  if(avMeas < 3){  // if the measurement is less than 3 cm from the top i.e. full
-  //Serial.print("Average Distance is ");
-  //Serial.print(avMeas);
-  //Serial.println(" cm");
-  Serial.println("Recycling-Bin is full");
+  if(avMeas < 10){    // if distance smaller than 10 cm - its full
+  //sensor.print("Average Distance is ");
+  //sensor.print(avMeas);
+  //sensor.println(" cm");
+  //sensor.println("Recycling-Bin is full");
+    blue.println("Recycling-Bin No.1 is full");  // send the app that is full
   }
-  //else if(avMeas > 3){
-   //   Serial.println("Recycling-Bin is not full");
+  //else if(avMeas > 10){
+   //   sensor.println("Recycling-Bin is not full");
   //}
 
   }
 //else {
- // Serial.println(" it is not the time");
+ // sensor.println(" it is not the time");
+ // blue.println(" it is not the time");
+
  // delay(1000);
  // }
+ checkState();
+
 }
